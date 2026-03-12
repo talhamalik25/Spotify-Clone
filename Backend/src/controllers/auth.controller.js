fe7e4a89c3e942412cbe40a3c8e87a7f46321fb8
Talha Malik <talhamalik25>
2026-03-12 17:27:25 +0500

    add

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -49,6 +49,47 @@ async function registerUser(req, res) {
     });
 }
 
+async function loginUser(req, res) {
+
+    const {username, password, email} = req.body;
+
+    const user = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    });
+
+    if (!user) {
+        return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+            role: user.role
+        },
+        process.env.JWT_SECRET)
+    res.cookie("token", token);
+
+    res.status(200).json({
+        message: "User logged in successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        }
+});
+}
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 };
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -4,6 +4,7 @@ const authController = require("../controllers/auth.controller")
 
 
 router.post('/register', authController.registerUser);
+router.post('/login', authController.loginUser);
 
 
 module.exports = router;
